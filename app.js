fe95ad31c1152460e1891a4e7107c8ccbddf224b
Manash Ranjan <mahanandmanash91@gmail.com>
2024-06-15 22:06:26 +0530

    add 300 port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
