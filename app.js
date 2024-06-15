ed8d1939573c94bc0010a0a86007a38c1a410440
Manash Ranjan <mahanandmanash91@gmail.com>
2024-06-15 23:19:57 +0530

    css build

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
+// import ViteExpress from "vite-express";
 const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
+const viteExpress = require('vite-express');
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
